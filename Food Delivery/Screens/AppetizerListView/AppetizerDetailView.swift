a419d702c65f62fa51ff2cb2860b7fbc91243ec1
NexusUA <pbogomolny93@gmal.com>
2022-11-03 17:37:28 +0100

    DetailView UI Setup and Refactor

--- Food Delivery/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Food Delivery/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -8,13 +8,80 @@
 import SwiftUI
 
 struct AppetizerDetailView: View {
+    
+    let appetizer: Appetizer
+    @Binding var isShowingDetail: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Image
+            AppetizerRemoteImage(urlString: appetizer.imageURL)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 225)
+            
+            // Info about food
+            VStack {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+                
+                // Nutrition info
+                HStack(spacing: 40) {
+                    NutritionInfo(title: "Calories", value: appetizer.calories)
+                    NutritionInfo(title: "Carbs", value: appetizer.carbs)
+                    NutritionInfo(title: "Protein", value: appetizer.protein)
+                }
+            }
+            
+            Spacer()
+            
+            // Button
+            Button {
+
+            } label: {
+                APButton(title: "$\(appetizer.price, specifier: "%.2f") - Add to Order")
+            }
+            .padding(.bottom, 30)
+        }
+        .frame(width: 300, height: 525)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40)
+        .overlay(Button {
+            isShowingDetail = false
+        } label: {
+            XDismissButton()
+        }, alignment: .topTrailing)
     }
 }
 
 struct AppetizerDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        AppetizerDetailView()
+        AppetizerDetailView(appetizer: MockData.sampleAppetizer, isShowingDetail: .constant(true))
+    }
+}
+
+struct NutritionInfo: View {
+    
+    let title: String
+    let value: Int
+    
+    var body: some View {
+        
+        VStack(spacing: 5) {
+            Text(title)
+                .bold()
+                .font(.caption)
+            
+            Text("\(value)")
+                .foregroundColor(.secondary)
+                .fontWeight(.semibold)
+                .italic()
+        }
     }
 }
--- Food Delivery/Screens/AppetizerListView/AppetizerListView.swift
+++ Food Delivery/Screens/AppetizerListView/AppetizerListView.swift
@@ -18,11 +18,22 @@ struct AppetizerListView: View {
                 
                 List(viewModel.appetizers) { appetizer in
                     AppetizerRowView(appetizer: appetizer)
+                        .onTapGesture {
+                            viewModel.selectedAppetizer = appetizer
+                            viewModel.isShowingDetail = true
+                        }
                 }
                 .navigationTitle("Appetizers")
+                .disabled(viewModel.isShowingDetail)
             }
             .onAppear {
                 viewModel.getAppetizers()
+            }
+            .blur(radius: viewModel.isShowingDetail ? 20 : 0)
+            
+            if viewModel.isShowingDetail {
+                AppetizerDetailView(appetizer: (viewModel.selectedAppetizer ?? MockData.sampleAppetizer),
+                                    isShowingDetail: $viewModel.isShowingDetail)
             }
             
             if viewModel.isLoading {
